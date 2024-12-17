72b8514a92b268b1beb96550ae2d61215d011dfe
Meet Tomar <meetm7.mt@gmail.com>
2024-12-17 18:49:22 +0530

    scoring system added

--- Exercise/guess-the-num.js
+++ Exercise/guess-the-num.js
@@ -1,14 +1,14 @@
-let randomNumber = Math.floor(Math.random() * 10) + 1;
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 let guess;
 let attempt = 0;
-let maxAttempt = 6;
+let maxAttempt = 90;
 
 do{
-  guess = parseInt(prompt("Guess a number between 1 to 10"),10);
+  guess = parseInt(prompt("Guess a number between 1 to 100"),10);
     attempt++
-  if(isNaN(guess) || guess > 10 || guess < 1){
+  if(isNaN(guess) || guess > 100 || guess < 1){
     // alert(`Try Again, Your Guess: ${guess}, Computer Guess: ${randomNumber}`);
-    alert(`Your guess is Invalid, Try again and guess in between 1 to 10`);
+    alert(`Your guess is Invalid, Try again and guess in between 1 to 100`);
   }
   else if(guess > randomNumber){
     alert("too high");
@@ -18,6 +18,7 @@ do{
   }
   else if(guess===randomNumber){
     alert ("congratualtions! Your guess is correct")
+    alert(`Your Score is: ${100 - attempt}`);
     break;
   }
   if(attempt===maxAttempt){
